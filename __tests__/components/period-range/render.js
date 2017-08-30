17bd970a3e5a5790b5593a2644f1c03ee75c355a
UNIVERSE\eklimenko <Eugene.Klimenko@dataart.com>
2017-08-30 17:39:53 +0300

    period-range fix for the empty period

--- __tests__/components/period-range/render.js
+++ __tests__/components/period-range/render.js
@@ -32,5 +32,31 @@ describe('Period range rendering test', () => {
         expect(range.text()).toBe('1 January — 5 February');
     });
 
+    it('should render no info when period dates are nulls', () => {
+        const {props, periodRange} = setup(null, null);
+        let range = periodRange.find('[className="holder"]');
+        expect(range.text()).toBe('no info');
+    });
+
+    it('should render no info when period dates are undefined', () => {
+        const {props, periodRange} = setup(undefined, undefined);
+        let range = periodRange.find('[className="holder"]');
+        expect(range.text()).toBe('no info');
+    });
+
+    it('should render no info when period is null', () => {
+        const periodRange = shallow(<PeriodRange period={null}/>)
+        let range = periodRange.find('[className="holder"]');
+        expect(range.text()).toBe('no info');
+    });
+
+    it('should render today when period is null', () => {
+        const periodRange = shallow(<PeriodRange period={undefined}/>)
+        let range = periodRange.find('[className="holder"]');
+        let today = new Date();
+        expect(range.text()).toBe(
+            `${today.getDate()} — ${today.getDate()} ${today.toLocaleString('en-us', { month: 'long' })}`
+        );
+    });
 });
 
